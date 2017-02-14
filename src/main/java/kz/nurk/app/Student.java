72c9463fed2401d2a31b639c1ddf89c6922eb178
ДосжанН <nurkanat.doszhan@mail.ru>
2017-02-14 18:06:21 +0300

    class student

---
+++ src/main/java/kz/nurk/app/Student.java
@@ -0,0 +1,19 @@
+package kz.nurk.app;
+
+public class Student {
+	
+	public String name;
+	public String group;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+}
